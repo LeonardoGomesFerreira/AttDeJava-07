484d76e42f23440e3798b818bad795ca027871f1
Leonardo G Ferreira <leozinhogameplays156@gmail.com>
2026-03-24 01:20:44 -0300

    REPOSITORY

---
+++ src/main/java/com/Att/Gerenciamento/de/Fornecedores/repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.Att.Gerenciamento.de.Fornecedores.repository;
+
+import com.Att.Gerenciamento.de.Fornecedores.entity.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+}
