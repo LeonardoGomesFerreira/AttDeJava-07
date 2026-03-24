69ef4f8caf75c74699cae8eb5dc7707cd0bfe0dd
Leonardo G Ferreira <leozinhogameplays156@gmail.com>
2026-03-24 01:18:42 -0300

    entity

---
+++ src/main/java/com/Att/Gerenciamento/de/Fornecedores/GerenciamentoDeFornecedoresApplication.java
@@ -0,0 +1,13 @@
+package com.Att.Gerenciamento.de.Fornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeFornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeFornecedoresApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Att/Gerenciamento/de/Fornecedores/entity/Fornecedor.java
@@ -0,0 +1,63 @@
+package com.Att.Gerenciamento.de.Fornecedores.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "cnpj_fornecedor")
+    private String cnpj;
+
+    private String contato;
+
+    // Construtores
+    public Fornecedor() {}
+
+    public Fornecedor(Long id, String nomeFantasia, String cnpj, String contato) {
+        this.id = id;
+        this.nomeFantasia = nomeFantasia;
+        this.cnpj = cnpj;
+        this.contato = contato;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
